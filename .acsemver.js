f112eda15c8f2c3bc8fe21bd5acc17fe487d3894
Mark Poepping <mark.poepping@mmpro.de>
2020-11-19 10:27:54 +0100

    chore(Limiter): Use ac-semantic-release | MP
    Use ac-semantic-release

---
+++ .acsemver.js
@@ -0,0 +1,9 @@
+module.exports = {
+  repository: {
+    url: 'https://github.com/mmpro/ac-ratelimiter'
+  },
+  changelogFile: __dirname + '/CHANGELOG.md',
+  sections: [
+    {name: 'Limiter' }
+  ]
+}
