8f25561c31f8618d45affac87baa862ca1764c42
Mark Poepping <mark.poepping@mmpro.de>
2021-10-09 12:32:34 +0200

    fix(Limiter): Package updates | MP
    Package updates

--- .acsemver.js
+++ .acsemver.js
@@ -1,6 +1,6 @@
 module.exports = {
   repository: {
-    url: 'https://github.com/mmpro/ac-ratelimiter'
+    url: 'https://github.com/admiralcloud/ac-ratelimiter'
   },
   changelogFile: __dirname + '/CHANGELOG.md',
   sections: [
