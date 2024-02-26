05828c26cdd56dde87258267d014c0019532647e
Mark Poepping <mark.poepping@admiralcloud.com>
2024-02-26 08:51:22 +0100

    [develop] fix(Limiter): Package updates | MP
    Package updates
    Related issues:
    - undefined/browse/develop

---
+++ .ncurc.js
@@ -0,0 +1,7 @@
+// List packages for minor updates
+const minorUpdatePackages = ['chai']
+module.exports = {
+  target: packageName => {
+    return minorUpdatePackages.includes(packageName) ? 'minor' : 'latest'
+  }
+}
